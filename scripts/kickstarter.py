12fcb275e173e404c24858b49a410743eef3ac6c
kentom <tmizuno@tozz.jp>
2024-07-06 16:02:17 +0900

    kickstarterから取得するコードの作成

--- scripts/kickstarter.py
+++ scripts/kickstarter.py
@@ -7,71 +7,59 @@ from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-# from slack_sdk.webhook import WebhookClient
-# from termcolor import colored
+from slack_sdk.webhook import WebhookClient
 
 
 threshold_day = os.environ.get("THRESHOLD_DAY", 1)
 threshold = datetime.now() - timedelta(days=threshold_day)
-user_agent = os.environ.get("USER_AGENT", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
-# slack_webhook_url = os.environ.get("SLACK_WEBHOOK_URL", None)
-# slack_channel = os.environ.get("SLACK_CHANNEL", None)
-# slack_username = os.environ.get("SLACK_USERNAME", None)
-# slack_user_icon = os.environ.get("SLACK_USER_ICON", None)
-# slack_client = WebhookClient(slack_webhook_url) if slack_webhook_url else None
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+slack_webhook_url = os.environ.get("SLACK_WEBHOOK_URL", 1)
+
+slack_client = WebhookClient(slack_webhook_url) if slack_webhook_url else None
 
 print("-"*10)
 print(f"user-agent: {user_agent}")
 print(f"threshold day: {threshold}")
-# print(f"slack channel: {slack_channel}")
-# print(f"slack username: {slack_username}")
-# print(f"slack user icon: {slack_user_icon}")
-# if slack_webhook_url is None:
-#     print("disable slack notification")
-# else:
-#     print("enable slack notification")
 print("-"*10)
 
 params = {
     "category_id": 337,
-    # "woe_id": 23424856,
     "sort": "newest",
     "seed": "2866362",
     "page": 1,
-    "format": "json",
 }
 
 driver = webdriver.Remote(
-    command_executor="http://selenium:44444/wd/hub",
+    command_executor="http://selenium:4444/wd/hub",
     options=webdriver.ChromeOptions()
 )
 url = f"https://www.kickstarter.com/discover/advanced.json?{urllib.parse.urlencode(params)}"
 driver.get(url)
 
-
 try:
-    data = json.loads(BeautifulSoup(driver.page_source, "lxml").find("body").text)
-except Exception:
+    data = json.loads(BeautifulSoup(driver.page_source, "html.parser").find("body").text)
+    for project in data["projects"]:
+        name = project["name"]
+        launched_at = datetime.fromtimestamp(project["launched_at"])
+        url = project["urls"]["web"]["project"]
+
+        print(f"check: {name} ({launched_at})", end=" ")
+        if threshold < launched_at:
+            if slack_webhook_url is None:
+                print("skip(slack disabled)")
+                continue
+
+            print("post")
+            msg = f"Kickstarterで {name} のクラウドファンディングが開始しました\n{url}"
+            slack_client.send(
+                text=msg,
+                unfurl_links=True,
+            )
+        else:
+            print("skip")
+
+except Exception as e:
+    print(e)
     exit(1)
-
-# for project in data["projects"]:
-#     name = project["name"]
-#     launched_at = datetime.fromtimestamp(project["launched_at"])
-#     url = project["urls"]["web"]["project"]
-
-#     print(f"check: {name} ({launched_at})", end=" ")
-#     if threshold < launched_at:
-#         if slack_webhook_url is None:
-#             print(colored("skip(slack disabled)", "red"))
-#             continue
-
-#         print(colored("post", "green"))
-#         msg = f"{name} のクラウドファンディングが開始しました\n{url}"
-#         slack_client.send_dict({k: v for k, v in {
-#             "text": msg,
-#             "channel": slack_channel,
-#             "username": slack_username,
-#             "icon_emoji": slack_user_icon,
-#         }.items() if v is not None})
-#     else:
-#         print(colored("skip", "red"))
+finally:
+    driver.quit()
